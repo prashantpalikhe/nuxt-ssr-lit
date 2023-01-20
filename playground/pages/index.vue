87a03ef06813d629c57c54e2897f2ac5dd159f99
Prashant Palikhe <prashpal09@gmail.com>
2023-01-20 10:09:52 +0100

    fix: elementProperties is a Map and not a POJO (#47)

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -3,13 +3,13 @@
     <my-element>I am a SSR-ed Lit element</my-element>
 
     <my-accordion foo="bar">
-      <my-accordion-item title="foo bar" open>
+      <my-accordion-item id="firstAccordionItem" title="foo bar" open>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt quas voluptatem quis commodi excepturi,
         aspernatur quia doloribus suscipit sed illo, omnis quisquam sapiente iusto adipisci amet aperiam necessitatibus
         deserunt.
       </my-accordion-item>
 
-      <my-accordion-item title="baz qux">
+      <my-accordion-item id="secondAccordionItem" title="baz qux">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium a quod facilis. Quam at doloremque, amet
         ullam earum iusto quia rem nihil impedit itaque labore vel neque repellat, inventore quaerat.
       </my-accordion-item>
