e94cb465552794d8f0c58eefb79a2b6246ff1e2b
Prashant Palikhe <prashpal09@gmail.com>
2023-02-25 09:20:15 +0530

    fix: incorrect shadowroot attribute in DSD template, should be shadowrootmode

--- tests/playground/basic.spec.ts
+++ tests/playground/basic.spec.ts
@@ -11,21 +11,21 @@ describe("ssr", async () => {
 
   it("renders the index page with a single simple element", async () => {
     const html = await $fetch("/");
-    expect(html).toContain('<my-element defer-hydration="true"><template shadowroot="open"><style>');
+    expect(html).toContain('<my-element defer-hydration="true"><template shadowrootmode="open"><style>');
     expect(html).toContain("<!--/lit-part--></template>I am a SSR-ed Lit element</my-element>");
   });
 
   it("renders multiple different element tags when supplied", async () => {
     const html = await $fetch("/multiple-different-element-tags");
-    expect(html).toContain('<my-element defer-hydration="true"><template shadowroot="open"><style>');
+    expect(html).toContain('<my-element defer-hydration="true"><template shadowrootmode="open"><style>');
     expect(html).toContain("<!--/lit-part--></template>I am a SSR-ed Lit element</my-element>");
-    expect(html).toContain('<simple-button defer-hydration="true"><template shadowroot="open"><style>');
+    expect(html).toContain('<simple-button defer-hydration="true"><template shadowrootmode="open"><style>');
     expect(html).toContain("<!--/lit-part--></template>And so am I</simple-button>");
   });
 
   it("renders nested lit components", async () => {
     const html = await $fetch("/nested-lit-element-in-slot");
-    expect(html).toContain('<my-element defer-hydration="true"><template shadowroot="open"><style>');
+    expect(html).toContain('<my-element defer-hydration="true"><template shadowrootmode="open"><style>');
     expect(html).toContain(
       '<!--/lit-part--></template><div slot="prepend">I am a Lit element within another Lit element</div></my-element>'
     );
@@ -33,7 +33,7 @@ describe("ssr", async () => {
 
   it("renders component attributes", async () => {
     const html = await $fetch("/simple-element");
-    expect(html).toContain('<simple-button disabled defer-hydration="true"><template shadowroot="open"><style>');
+    expect(html).toContain('<simple-button disabled defer-hydration="true"><template shadowrootmode="open"><style>');
   });
 
   it("renders boolean property correctly on server", async () => {
