c13933cea6aeefed75810ba0ac089aea4845d930
Prashant Palikhe <prashpal09@gmail.com>
2023-02-27 14:54:05 +0530

    Update src/runtime/plugins/polyfill.client.ts
    Co-authored-by: Steve Workman <steve.workman@maersk.com>

--- src/runtime/plugins/polyfill.client.ts
+++ src/runtime/plugins/polyfill.client.ts
@@ -2,8 +2,7 @@ import { defineNuxtPlugin } from "#imports";
 
 export default defineNuxtPlugin(async () => {
   // shadowRootMode is the standardized attribute.
-  // but to support current Chrome, we need to check for shadowRoot as well.
-  // TODO: we can remove shadowRoot when Chrome 111 is released or is at least the last 2 released versions.
+  // Chromium 90-111 supported shadowRoot, so we check for that as well.
   const hasNativeDsd =
     Object.hasOwnProperty.call(HTMLTemplateElement.prototype, "shadowRootMode") ||
     Object.hasOwnProperty.call(HTMLTemplateElement.prototype, "shadowRoot");
