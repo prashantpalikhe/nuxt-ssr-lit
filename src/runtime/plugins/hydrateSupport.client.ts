275ffc50729afd704e768fa70178b3a5ae601f36
Prashant Palikhe <prashpal09@gmail.com>
2023-04-06 23:50:05 +0530

    chore: upgrade dependency @lit-labs/ssr@3.1.1

--- src/runtime/plugins/hydrateSupport.client.ts
+++ src/runtime/plugins/hydrateSupport.client.ts
@@ -1,4 +1,4 @@
-import "lit/experimental-hydrate-support.js";
+import "@lit-labs/ssr-client/lit-element-hydrate-support.js";
 import { defineNuxtPlugin } from "#imports";
 
 export default defineNuxtPlugin(() => {});
