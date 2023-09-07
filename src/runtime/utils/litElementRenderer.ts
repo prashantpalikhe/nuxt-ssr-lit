e36e88e1de224958e8cd471f5fe4a70700e3f982
Prashant Palikhe <prashpal09@gmail.com>
2023-09-07 09:18:17 +0200

    feat: change the way we render the component on server (#93)

---
+++ src/runtime/utils/litElementRenderer.ts
@@ -0,0 +1,59 @@
+import { LitElementRenderer } from "@lit-labs/ssr/lib/lit-element-renderer.js";
+import { getCustomElementConstructor, isCustomElementTag } from "./customElements";
+
+export function createLitElementRenderer(tagName: string, props: Record<string, unknown>): LitElementRenderer | null {
+  if (!isCustomElementTag(tagName)) {
+    return null;
+  }
+
+  const renderer = new LitElementRenderer(tagName);
+  return attachPropsToRenderer(renderer, props);
+}
+
+export function getShadowContents(renderer: LitElementRenderer): string {
+  return iterableToString(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    renderer.renderShadow({
+      elementRenderers: [LitElementRenderer],
+      customElementInstanceStack: [],
+      customElementHostStack: []
+    }) as Iterable<string>
+  );
+}
+
+function attachPropsToRenderer(renderer: LitElementRenderer, props: Record<string, unknown>): LitElementRenderer {
+  const customElementConstructor = getCustomElementConstructor(renderer.tagName);
+
+  if (props) {
+    for (const [key, value] of Object.entries(props)) {
+      // check if this is a reactive property
+      if (
+        customElementConstructor !== null &&
+        typeof customElementConstructor !== "string" &&
+        key in customElementConstructor.prototype
+      ) {
+        const isBooleanProp = customElementConstructor.elementProperties.get(key)?.type === Boolean;
+
+        if (isBooleanProp && value === "") {
+          // handle key only boolean props e.g. <my-element disabled></my-element>
+          renderer.setProperty(key, true);
+        } else {
+          renderer.setProperty(key, value);
+        }
+      } else {
+        renderer.setAttribute(key, value as string);
+      }
+    }
+  }
+
+  return renderer;
+}
+
+function iterableToString(iterable: Iterable<string>) {
+  let s = "";
+  for (const i of iterable) {
+    s += i;
+  }
+  return s;
+}
