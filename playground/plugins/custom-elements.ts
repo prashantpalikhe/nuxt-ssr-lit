b4390e5f1871b3175c0a17619676c560415b97b0
Augustine Kim <augustinekim@google.com>
2023-11-06 18:24:13 -0800

    Add test

--- playground/plugins/custom-elements.ts
+++ playground/plugins/custom-elements.ts
@@ -2,5 +2,6 @@ import { defineNuxtPlugin } from "#app";
 import "@/wc/my-element";
 import "@/wc/simple-button";
 import "@/wc/accordion";
+import "@/wc/simple-parent";
 
 export default defineNuxtPlugin(() => {});
