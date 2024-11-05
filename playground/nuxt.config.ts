fe598b3f02e4341ba2692d5cd5880b3a1a746395
Prashant Palikhe <prashpal09@gmail.com>
2024-11-05 10:10:38 +0100

    fix: update deps (#165)

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,5 +3,6 @@ import NuxtSsrLit from "..";
 
 export default defineNuxtConfig({
   modules: [[NuxtSsrLit, { litElementPrefix: ["my-", "simple-"] }]],
-  sourcemap: process.env.NODE_ENV === "test" ? false : { client: true, server: true }
+  sourcemap: process.env.NODE_ENV === "test" ? false : { client: true, server: true },
+  compatibilityDate: "2024-11-05"
 });
