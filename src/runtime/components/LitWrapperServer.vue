9aefbc839785c3eafc10031f3afc55ddcfb1b37c
Konnor Rogers <konnor5456@gmail.com>
2026-06-19 11:46:35 -0400

    upgrade to v4 of lit ssr

--- src/runtime/components/LitWrapperServer.vue
+++ src/runtime/components/LitWrapperServer.vue
@@ -3,9 +3,51 @@ import { defineComponent, ssrUtils, mergeProps } from "vue";
 import type { ComponentInternalInstance } from "vue";
 import { ssrRenderVNode, ssrRenderAttrs } from "@vue/server-renderer";
 import { LitElementRenderer } from "@lit-labs/ssr/lib/lit-element-renderer.js";
+import { collectResultSync } from "@lit-labs/ssr/lib/render-result.js";
 import { createLitElementRenderer } from "../utils/litElementRenderer";
 
 type PushFn = Parameters<typeof ssrRenderVNode>[0];
+
+/**
+ * https://github.com/lit/lit/blob/ada3ffce30cdb6a2f9a9d476767eaf747a0b2667/packages/labs/ssr/src/lib/render-result.ts#L95C1-L129C3
+ * Joins a RenderResult or ThunkedRenderResult into a string synchronously.
+ *
+ * This function throws if a RenderResult contains a Promise.
+ */
+const collectResultSync = (
+  result: RenderResult | ThunkedRenderResult,
+  throwOnPromise: boolean = true
+): string => {
+  let str = '';
+  for (const chunk of result) {
+    let value:
+      | string
+      | Thunk
+      | Promise<string | RenderResult | ThunkedRenderResult>
+      | RenderResult
+      | ReturnType<Thunk> = chunk;
+
+    while (typeof value === 'function') {
+      value = value();
+    }
+
+    if (typeof value === 'string') {
+      str += value;
+    } else if (Array.isArray(value)) {
+      str += collectResultSync(value);
+    } else if (value !== undefined) {
+      if (throwOnPromise) {
+        throw new Error(
+          'Promises not supported in collectResultSync. ' +
+            'Please use collectResult.'
+        );
+      }
+      // Rather than error the whole thing, just silently fail it.
+    }
+  }
+
+  return str;
+};
 
 export default defineComponent({
   ssrRender(
@@ -33,14 +75,19 @@ export default defineComponent({
         `<${litElementTagName}${attributes} defer-hydration="true"><template shadowrootmode="open" shadowroot="open">`
       );
 
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      for (const shadowContent of renderer.renderShadow({
+      const shadowContents = renderer.renderShadow({
         elementRenderers: [LitElementRenderer],
         customElementInstanceStack: [renderer],
-        customElementHostStack: [renderer]
-      }) as Iterable<string>) {
-        push(shadowContent);
+        customElementHostStack: [renderer],
+        eventTargetStack: [renderer.element],
+        slotStack: [],
+        deferHydration: false,
+      });
+
+
+      if (shadowContents != null) {
+      // I'm assuming this renderer needs to be synchronous. If we support async, we can remove our custom `collectResultSync` and swap to using Lit's `await collectResult(shadowContents)`
+        push(collectResultSync(shadowContents, false));
       }
 
       push(`</template>`);
@@ -64,3 +111,4 @@ export default defineComponent({
   }
 });
 </script>
+
